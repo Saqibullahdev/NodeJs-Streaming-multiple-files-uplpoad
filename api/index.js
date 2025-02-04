6f0f1fce9614c7843cf905a44cca1d00a277265a
saqib ullah <22pwbcs0916@uetpeshawar.edu.pk>
2025-02-04 13:49:13 +0500

    2nd commit

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const cors=require('cors');
 app.use(cors());
-
 app.get('/', (req, res) => {
+    res.send('Hello World');
+}
+app.get('/test', (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Transfer-Encoding', 'chunked');
 
